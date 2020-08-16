85932afed1b48241963a0b72156311e2cc5e656d
selmanbey <selmannur@yahoo.com>
2020-08-16 09:22:59 +0100

    aims to fix a scrolling issue for Galaxy S7

--- pages/connect.js
+++ pages/connect.js
@@ -1,5 +1,11 @@
+import styles from "../styles/SharedStyles.module.css";
+
 const francescaGotti = ({ router }) => {
-  return <section></section>;
+  return (
+    <section>
+      <article className={styles.article}>connect</article>
+    </section>
+  );
 };
 
 export default francescaGotti;
